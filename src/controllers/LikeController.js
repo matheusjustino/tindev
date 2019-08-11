14246764c4459e05485e4b019ce64aea7487d526
matheusjustino <matheus.justino@ccc.ufcg.edu.br>
2019-08-11 20:37:35 -0300

    adicionando socketio

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -2,6 +2,7 @@ const Dev = require('../models/Dev');
 
 module.exports = {
     async store(req, res) {
+        console.log(req.io, req.connectUsers);
         // obtendo id do usuário que está recebendo o like
         const { devId } = req.params;
         // obtendo o id do usuário que está logado e dando o like
@@ -9,7 +10,7 @@ module.exports = {
 
         const loggedDev = await Dev.findById(user); // instância do dev logado
         const targetDev = await Dev.findById(devId); // instância do dev que está recebendo o like
-        
+
         // verificando se o dev alvo existe
         if (!targetDev) {
             return res.status(400).json({ error: 'Dev not exists' });
@@ -17,7 +18,16 @@ module.exports = {
 
         // verificando se o alvo já deu like no usuário logado, assim avisando se deu match
         if (targetDev.likes.includes(loggedDev._id)) {
-            console.log('deu match');
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            if (loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         }
 
         // salvando no array de likes
--- src/server.js
+++ src/server.js
@@ -6,19 +6,39 @@ const cors = require('cors');
 const routes = require('./routes');
 
 // criando o servidor
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+// fazendo o servidor receber requisições http e websocket
+const io = require('socket.io')(server);
+// armazenando id e socket do usuário
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+  
+    connectedUsers[user] = socket.id;
+  });
+  
 
 // conexão com o banco de dados
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-r58uk.mongodb.net/omnistack8?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-// utilizando cors para permitir que a aplicação seja acessada de qualquer endereço
-server.use(cors());
-// para o servidor entender o formato json
-server.use(express.json());
 
-server.use(routes);
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+  
+    return next();
+  });
+
+// utilizando cors para permitir que a aplicação seja acessada de qualquer endereço
+app.use(cors());
+// para o servidor entender o formato json
+app.use(express.json());
+
+app.use(routes);
 
 
 // subindo o servidor
